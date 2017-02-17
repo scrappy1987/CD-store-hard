9a0dd7c11a976aecd888be07805c6f3570983a58
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-17 10:29:45 +0000

    Added test skelton

---
+++ src/main/java/com/qa/cdstore/test/CDTest.java
@@ -0,0 +1,59 @@
+package com.qa.cdstore.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CDTest {
+
+	@Test
+	public void testCDIntStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCDStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArtist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetArtist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGenre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGenre() {
+		fail("Not yet implemented");
+	}
+
+}
