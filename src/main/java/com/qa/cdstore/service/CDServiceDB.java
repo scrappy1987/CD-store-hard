27bf1a5ec20b142bc6bcb0b4e6ec915c65776a4d
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-17 12:35:09 +0000

    Added initial implementation of CDService interface in CDServiceDB
    Minor change to CDService interface to add String return type

--- src/main/java/com/qa/cdstore/service/CDServiceDB.java
+++ src/main/java/com/qa/cdstore/service/CDServiceDB.java
@@ -1,10 +1,31 @@
 package com.qa.cdstore.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.qa.cdstore.model.CD;
+import com.qa.cdstore.util.UtilJSON;
 
 @Stateless
 @Default
-public class CDServiceDB {
-
+public class CDServiceDB implements CDService {
+	
+	private EntityManager em;
+	
+	@Inject
+	private UtilJSON helper;
+	
+	public String getCDs() {
+		List<CD> cds = new ArrayList<CD>();
+		cds = em.createQuery("SELECT cd FROM CD cd").getResultList();
+		if (cds != null)
+			return helper.toJson(cds);
+		else 
+			return "";
+	}
 }
