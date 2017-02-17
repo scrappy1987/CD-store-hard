d10cc5a0b0a237d40e07fbd7c8402d059f5aa5ac
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-17 13:02:01 +0000

    Added getAllCDs to the endpoint
    Next to add add cd to endpoint and then test via postman

--- src/main/java/com/qa/cdstore/rest/CDRestEndPoint.java
+++ src/main/java/com/qa/cdstore/rest/CDRestEndPoint.java
@@ -1,5 +1,25 @@
 package com.qa.cdstore.rest;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.qa.cdstore.service.CDService;
+
+@Path("/CDs")
+@RequestScoped
 public class CDRestEndPoint {
 
+	@Inject
+	CDService cdService;
+	
+	@GET
+	@Path("/json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllCDs() {
+		return cdService.getCDs();
+	}
 }
