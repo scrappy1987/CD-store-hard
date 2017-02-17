517ea76b83c2210580279ab31f522aba7e29c017
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-17 14:38:47 +0000

    Remove request scoped and add add new cd exposure on rest

--- src/main/java/com/qa/cdstore/rest/CDRestEndPoint.java
+++ src/main/java/com/qa/cdstore/rest/CDRestEndPoint.java
@@ -1,8 +1,8 @@
 package com.qa.cdstore.rest;
 
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,16 +10,22 @@ import javax.ws.rs.core.MediaType;
 import com.qa.cdstore.service.CDService;
 
 @Path("/CDs")
-@RequestScoped
 public class CDRestEndPoint {
 
 	@Inject
-	CDService cdService;
+	private CDService cdService;
 	
 	@GET
 	@Path("/json")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getAllCDs() {
 		return cdService.getCDs();
+	}
+	
+	@POST
+	@Path("/json")
+	@Produces({ "application/json" })
+	public String addNewCD(String cdJson) {
+		return cdService.addCD(cdJson);
 	}
 }
